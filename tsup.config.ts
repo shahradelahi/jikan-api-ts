2f08f76976bffb42008dc715796a694e8e8258b8
Shahrad Elahi <shahradq@gmail.com>
2024-08-13 08:49:00 +0300

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig([
   {
     clean: true,
-    sourcemap: true,
     dts: true,
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
